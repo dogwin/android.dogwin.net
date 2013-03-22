6bddc97a6e3d8fb2e6a517ae7ad123b41f877873
darren <darren.miao@e-graphics.com>
2013-03-22 17:45:08 +0800

    dogwin learn

--- Dogwin/src/dogwin/net/apps/Dogwin.java
+++ Dogwin/src/dogwin/net/apps/Dogwin.java
@@ -1,15 +1,18 @@
 package dogwin.net.apps;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class Dogwin extends Activity {
-
+	
+	private final String Tag = "Main";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.dogwin);
+		Log.i(Tag, "onCreate");
 	}
 
 	@Override
@@ -18,5 +21,49 @@ public class Dogwin extends Activity {
 		getMenuInflater().inflate(R.menu.dogwin, menu);
 		return true;
 	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.i(Tag, "onDestroy");
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.i(Tag, "onPause");
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+		Log.i(Tag, "onRestart");
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.i(Tag, "onResume");
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Log.i(Tag, "onStart");
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.i(Tag, "onStop");
+	}
+	
+	
 
 }
