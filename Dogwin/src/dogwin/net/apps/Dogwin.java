67ae11a90f918145910c09e554e1e2ea7d44454c
darren <darren.miao@e-graphics.com>
2013-03-25 13:39:47 +0800

    intent static

--- Dogwin/src/dogwin/net/apps/Dogwin.java
+++ Dogwin/src/dogwin/net/apps/Dogwin.java
@@ -1,17 +1,45 @@
 package dogwin.net.apps;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Dogwin extends Activity {
 	
 	private final String Tag = "Main";
+	private Button button;
+	private EditText et;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.dogwin);
+		
+		
+		button = (Button)this.findViewById(R.id.bt);
+		et = (EditText)this.findViewById(R.id.sendmsg);
+		
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				/*Intent intent = new Intent(Dogwin.this,showmsg.class);
+				String etmsg = et.getText().toString();
+				intent.putExtra("etmsg", etmsg);
+				startActivity(intent);*/
+				Intent intent = new Intent();
+				intent.setClass(Dogwin.this, showmsg.class);
+				String etmsg = et.getText().toString();
+				showmsg.msgs = etmsg;
+				startActivity(intent);
+			}
+		});
+		
 		Log.i(Tag, "onCreate");
 	}
 
