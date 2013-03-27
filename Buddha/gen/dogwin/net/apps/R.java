54239b1cddfac7c786eefe915ab0482ee150e806
darren <darren.miao@e-graphics.com>
2013-03-27 17:47:41 +0800

    buddha

--- Buddha/gen/dogwin/net/apps/R.java
+++ Buddha/gen/dogwin/net/apps/R.java
@@ -24,12 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
-        public static final int menu_about_us=0x7f080003;
-        public static final int menu_exit=0x7f080005;
-        public static final int menu_send_adv=0x7f080002;
-        public static final int menu_tell_friend=0x7f080001;
-        public static final int menu_update=0x7f080004;
+        public static final int action_settings=0x7f080001;
+        public static final int analogClock1=0x7f080000;
+        public static final int menu_about_us=0x7f080004;
+        public static final int menu_exit=0x7f080006;
+        public static final int menu_send_adv=0x7f080003;
+        public static final int menu_tell_friend=0x7f080002;
+        public static final int menu_update=0x7f080005;
     }
     public static final class layout {
         public static final int buddha=0x7f030000;
